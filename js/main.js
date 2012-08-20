6ad49048f8b1d1f3644dd7b0f8606909c7050ac5
dbashford <dbashford@bericotechnologies.com>
2012-08-20 12:37:53 -0400

    commands

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-/* Author:
+!function ($) {
+  var $win = $(window)
+    , $nav = $('.subnav')
+    , navTop = $('.subnav').length && $('.subnav').offset().top - 40
+    , isFixed = 0
 
-*/
+  processScroll()
+
+  // hack sad times - holdover until rewrite for 2.1
+  $nav.on('click', function () {
+    if (!isFixed)
+      setTimeout(function () { $win.scrollTop($win.scrollTop() - 147) }, 10)
+    else
+      setTimeout(function () { $win.scrollTop($win.scrollTop() - 87) }, 10)
+  })
+
+  $win.on('scroll', processScroll)
+
+  function processScroll() {
+    var i, scrollTop = $win.scrollTop()
+    if (scrollTop >= navTop && !isFixed) {
+      isFixed = 1
+      $nav.addClass('subnav-fixed')
+    } else if (scrollTop <= navTop && isFixed) {
+      isFixed = 0
+      $nav.removeClass('subnav-fixed')
+    }
+  }
+}(window.jQuery)
